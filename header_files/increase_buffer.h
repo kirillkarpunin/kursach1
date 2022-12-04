17b70a59fd2728c705cbf92fa7e3b987e4256b34
kirill_karpunin <kirill.karpunin20014@gmail.com>
2022-12-04 20:01:51 +0300

    Counting words (unworking)

--- header_files/increase_buffer.h
+++
@@ -1,3 +0,0 @@
-void increase_buffer_text(text_t* ptr_Text);
-
-void increase_buffer_sent(text_t* ptr_Text);
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "header_files/structs.h"
 #include "header_files/get_text.h"
+#include "process_text.h"
 
 int main(){
     setlocale(LC_ALL, "ru_RU.UTF-8");
@@ -10,8 +11,11 @@ int main(){
 
     get_text(ptr_Text);
 
+    count_words(ptr_Text);
+
     for (int i = 0; i < ptr_Text->len; i++){
         fputws(ptr_Text->sent_arr[i].start, stdout);
+//        wprintf(L"\n%lu\n", ptr_Text->sent_arr[i].amount_of_words);
     }
 
     return 0;
