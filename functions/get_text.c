acae259b1f1bb64d0b3f117f8df0d8fdf36abbde
kirill_karpunin <kirill.karpunin20014@gmail.com>
2022-12-04 12:32:32 +0300

    sentence buffer increase (unworking)

--- functions/get_text.c
+++ functions/get_text.c
@@ -5,14 +5,17 @@ void get_text(text_t* ptr_Text){
     while (ptr_Text->len <= 5) {
 
         wchar_t c = 0;
+
         do {
             c = getwchar();
+
+            if (ptr_Text->sent_arr[ptr_Text->len - 1].len >= ptr_Text->sent_arr[ptr_Text->len -1].capacity - 1) {
+                increase_buffer_sent(ptr_Text);
+            }
+
             *(ptr_Text->sent_arr[ptr_Text->len - 1].start + ptr_Text->sent_arr[ptr_Text->len - 1].len) = c;
             ptr_Text->sent_arr[ptr_Text->len - 1].len += 1;
 
-            if (ptr_Text->sent_arr[ptr_Text->len - 1].len >= ptr_Text->sent_arr[ptr_Text->len -1].capacity - 2) {
-                break; //increase buffer
-            }
         } while (c != L'.');
 
         *(ptr_Text->sent_arr[ptr_Text->len - 1].start + ptr_Text->sent_arr[ptr_Text->len - 1].len) = '\0';
--- functions/increase_buffer.c
+++ functions/increase_buffer.c
@@ -16,3 +16,18 @@ void increase_buffer_text(text_t* ptr_Text){
         exit(0);
     }
 }
+
+void increase_buffer_sent(text_t* ptr_Text){
+    ptr_Text->sent_arr[ptr_Text->len -1].capacity += ptr_Text->increase_buffer_sent;
+    wprintf(L"len: %lu, capacity: %lu\n", ptr_Text->sent_arr[ptr_Text->len -1].len, ptr_Text->sent_arr[ptr_Text->len -1].capacity);
+    wchar_t* temp = realloc(ptr_Text->sent_arr[ptr_Text->len -1].start, ptr_Text->sent_arr[ptr_Text->len -1].capacity);
+
+    if (temp){
+        ptr_Text->sent_arr[ptr_Text->len -1].start = temp;
+
+    } else{
+        wprintf(L"Не удалось перевыделить память\n");
+        //destroy
+        exit(0);
+    }
+}
