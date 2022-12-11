20f454a6597415f89ccc1e4e2a11164adaa4ed30
kirill_karpunin <kirill.karpunin20014@gmail.com>
2022-12-11 12:49:37 +0300

    tsya

--- functions/replace_tsya.c
+++ functions/replace_tsya.c
@@ -2,6 +2,7 @@
 #include "../header_files/memory_interaction.h"
 
 #include <string.h>
+#include <wctype.h>
 
 void replace_tsya(text_t* ptr_Text) {
     wchar_t *substr1 = L"ться";
@@ -26,37 +27,47 @@ void replace_tsya(text_t* ptr_Text) {
                 break;
             } else {
                 if ((index2 == NULL || index1 < index2) && index1 != NULL) {
-                    index1++;
-                    *(index1) = substr2[1];
-                    index1++;
-                    *(index1) = substr2[2];
-                    index1++;
+                    if ((iswspace(*(index1+4)) || *(index1+4) == L',' || *(index1+4) == L'.')){ // 4 - len of ться
 
-                    memmove(index1, index1 + 1, (end - index1 + 1) * sizeof(wchar_t));
-                    ptr_Text->sent_arr[i].len--;
+                        index1++;
+                        *(index1) = substr2[1];
+                        index1++;
+                        *(index1) = substr2[2];
+                        index1++;
 
-                    end--;
-                    start = index1;
+                        memmove(index1, index1 + 1, (end - index1 + 1) * sizeof(wchar_t));
+                        ptr_Text->sent_arr[i].len--;
 
-                } else {
-                    if (ptr_Text->sent_arr[i].len == ptr_Text->sent_arr[i].capacity) {
-                        increase_buffer_sent(ptr_Text, i);
+                        end--;
+                        start = index1;
+                    } else {
+                        start = index1+4;
                     }
 
-                    index2++;
-                    *(index2) = substr1[1];
-                    index2++;
-                    *(index2) = substr1[2];
-                    index2++;
 
-                    memmove(index2 + 1, index2, (end - index2 + 1) * sizeof(wchar_t));
-                    ptr_Text->sent_arr[i].len++;
-                    end++;
+                } else {
+                    if ((iswspace(*(index2+3)) || *(index2+3) == L',' || *(index2+3) == L'.')) { // 3 - len of тся
+                        if (ptr_Text->sent_arr[i].len == ptr_Text->sent_arr[i].capacity) {
+                            increase_buffer_sent(ptr_Text, i);
+                        }
+                        index2++;
+                        *(index2) = substr1[1];
+                        index2++;
+                        *(index2) = substr1[2];
+                        index2++;
 
-                    *(index2) = substr1[3];
-                    index2++;
+                        memmove(index2 + 1, index2, (end - index2 + 1) * sizeof(wchar_t));
+                        ptr_Text->sent_arr[i].len++;
+                        end++;
 
-                    start = index2;
+                        *(index2) = substr1[3];
+                        index2++;
+
+                        start = index2;
+                    } else {
+                        start = index2 + 3;
+                    }
+
                 }
             }
         }
